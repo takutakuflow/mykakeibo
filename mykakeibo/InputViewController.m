f247702887d1416d6216b72605ad3d0f419382a6
ide <takutakuflow072@gmail.com>
2018-11-28 00:22:34 +0900

    入力画面のレイアウトを作成、テキストの入力処理は未実装

--- mykakeibo/InputViewController.m
+++ mykakeibo/InputViewController.m
@@ -8,7 +8,8 @@
 
 #import "InputViewController.h"
 
-@interface InputViewController ()
+@interface InputViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -17,21 +18,50 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 3;
 }
-*/
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // "cell"というkeyでcellデータを取得
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    // cellデータが無い場合、UITableViewCellを生成して、"cell"というkeyでキャッシュする
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    }
+    
+    switch (indexPath.row) {
+        case 0:
+            cell.textLabel.text = @"金額";
+            break;
+        case 1:
+            cell.textLabel.text = @"カテゴリー";
+            break;
+        case 2:
+            cell.textLabel.text = @"日付";
+            break;
+        default:
+            break;
+    }
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // cellがタップされた際の処理
+}
 
 @end
